8c1b454486c31cee4a30af1331a72d1bcc458f13
Rovshen Umudov <rovshen.umudov.triare@gmail.com>
2023-09-25 18:00:01 +0400

    Feature: Refactor - in progress.

--- components/CargoList/index.tsx
+++ components/CargoList/index.tsx
@@ -16,17 +16,17 @@ const CargoList: FC<ICargoList> = async ({ title, limit }) => {
   const cargos = await getCargos(session?.tokens?.access.token || '', limit);
 
   return (
-    <>
+    <div className="mb-5">
       <PageTitle title={title} />
       <div className="grid gap-5 grid-cols-3 max-[1200px]:grid-cols-2 max-[768px]:grid-cols-1">
         {cargos.data?.map((cargo) => <CargoItem data={cargo} key={crypto.randomUUID()} />)}
       </div>
       {cargos.data && limit && cargos.data.length > limit ? (
-        <div className="flex justify-center my-5">
+        <div className="flex justify-center mt-5">
           <LinkButton href="/cargos" label="View All" />
         </div>
       ) : null}
-    </>
+    </div>
   );
 };
 

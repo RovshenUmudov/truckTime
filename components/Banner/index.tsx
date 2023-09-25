d3a08f0e138af28e70975a2e30c6c64446f65628
Rovshen Umudov <rovshen.umudov.triare@gmail.com>
2023-09-25 11:45:04 +0400

    Feature: Refactor - in progress.

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -6,14 +6,14 @@ interface IBanner {
 }
 
 const Banner: FC<IBanner> = ({ src }) => (
-  <div className="h-[250px] max-[992px]:h-[200px] relative overflow-hidden rounded-md">
+  <div className="h-[250px] max-[992px]:h-[200px] max-[768px]:h-[150px] relative overflow-hidden rounded-md">
     <Image
       src={src}
       alt="banner"
       fill
       priority
       className="!h-auto max-[1300px]:!h-full max-[1250px]:!max-w-none max-[1250px]:!w-auto"
-      sizes="(max-width: 992px) 950px, (min-width: 1440px) 1360px"
+      sizes="(max-width: 992px) 100vw, (min-width: 1440px) 1360px"
       quality={90}
     />
   </div>

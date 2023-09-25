26bce1f63f03b49620c8d9d089c91db14800ca51
Rovshen Umudov <rovshen.umudov.triare@gmail.com>
2023-09-25 14:31:23 +0400

    Feature: Refactor - in progress.

---
+++ app/cargo/new/loading.tsx
@@ -0,0 +1,43 @@
+import { Skeleton } from '@/components/ui/skeleton';
+import { FC } from 'react';
+import Container from '@/components/Container';
+
+const Loading: FC = () => (
+  <>
+    <Skeleton className="h-24 max-[992px]:h-14 w-full" />
+    <Container>
+      <Skeleton className="h-[250px] w-full max-[1250px]:h-auto max-[1250px]:pt-[21%]" />
+      <Skeleton className="h-8 w-[300px] my-5" />
+      <div className="max-w-[700px] mt-5">
+        <div className="grid gap-5">
+          <div className="grid gap-5 grid-cols-2 max-[768px]:grid-cols-1">
+            <Skeleton className="h-10 w-full" />
+            <Skeleton className="h-10 w-full" />
+          </div>
+          <div className="grid gap-5 grid-cols-2 max-[768px]:grid-cols-1">
+            <Skeleton className="h-10 w-full" />
+            <Skeleton className="h-10 w-full" />
+          </div>
+          <div className="grid gap-5 grid-cols-2 max-[768px]:grid-cols-1">
+            <Skeleton className="h-10 w-full" />
+            <Skeleton className="h-10 w-full" />
+          </div>
+          <div className="grid gap-5 grid-cols-[1fr_170px] max-[768px]:grid-cols-1">
+            <Skeleton className="h-10 w-full" />
+            <Skeleton className="h-10 w-full" />
+          </div>
+          <div className="grid gap-5 grid-cols-2 max-[768px]:grid-cols-1">
+            <Skeleton className="h-10 w-full" />
+            <Skeleton className="h-10 w-full" />
+          </div>
+          <div className="flex justify-end gap-5">
+            <Skeleton className="h-10 w-[70px]" />
+            <Skeleton className="h-10 w-[70px]" />
+          </div>
+        </div>
+      </div>
+    </Container>
+  </>
+);
+
+export default Loading;

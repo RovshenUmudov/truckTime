9767a963a29f75597db93945bec1388561082ec5
Rovshen Umudov <rovshen.umudov.triare@gmail.com>
2023-09-26 02:32:36 +0400

    Feature: Refactor - in progress.

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -38,7 +38,7 @@ const Input = React.forwardRef<HTMLInputElement, IInputProps>(
           type={type}
           name={name}
           className={clsx(
-            'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 select-none '
+            'flex h-10 w-full text-left rounded-md border border-input bg-background px-3 py-2 select-none '
           + 'text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium '
           + 'placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring '
           + 'focus-visible:ring-offset-0 disabled:bg-secondary disabled:cursor-not-allowed disabled:opacity-50 ',
